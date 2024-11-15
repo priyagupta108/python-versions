0bbda2f30e6cfe464d9edc6fda1777a50ab420a9
Priya Gupta <147705955+priyagupta108@users.noreply.github.com>
2024-11-15 17:32:35 +0530

    Update macos-python-builder.psm1

--- builders/macos-python-builder.psm1
+++ builders/macos-python-builder.psm1
@@ -81,7 +81,7 @@ class macOSPythonBuilder : NixPythonBuilder {
             }
 
             if ($this.Version -gt "3.7.12") {
-                $configureString += " --with-tcltk-includes='-I /usr/local/opt/tcl-tk/include' --with-tcltk-libs='-L/usr/local/opt/tcl-tk/lib -ltcl9.0 -ltk9.0'"
+                $configureString += " --with-tcltk-includes='-I /usr/local/opt/tcl-tk/include' --with-tcltk-libs='-L/usr/local/opt/tcl-tk/lib -ltcl9 -ltk9'"
 	        }
 
             if ($this.Version -eq "3.7.17") {
